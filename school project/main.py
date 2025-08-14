e0be8ef7ed4286a835dc2e786423842175c8498f
matin357 <93083311+matin357@users.noreply.github.com>
2025-08-14 19:32:20 +0300

    Update main.py

--- school project/main.py
+++ school project/main.py
@@ -1,1 +1,12 @@
 import telebot
+file_with_tocken = open("tocken.txt", "r")
+tocken = file_with_tocken.readlines(1)
+bot = telebot.TeleBot(f'{tocken[0]}')
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(message.chat.id, 'Вітаємо у боті Liceum. Оберіть дію.')
+
+
+
+bot.polling(none_stop=True)
